d9f790321cf7dcbc0ace3a8d00ff15fa0a205332
KieranPritchard <KieranPritchard06@gmail.com>
2024-10-08 11:42:03 +0100

    Added main varibles to organiser

---
+++ Coding Project Organiser.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+#Main project directory path
+python_project_path = "/Users/kieranpritchard/Documents/Coding Projects/Python/Projects"
+html_css_js_project_path = "/Users/kieranpritchard/Documents/Coding Projects/HTML, CSS & JavaScript/Projects"
+cpp_project_path = "/Users/kieranpritchard/Documents/Coding Projects/C++/Projects"
+
+#File structure for each programing langage
+general_file_structure = [
+    "test",
+    "src",
+    ".build",
+    "tools",
+    "docs",
+    "dep",
+    "samples",
+    "res"
+]
