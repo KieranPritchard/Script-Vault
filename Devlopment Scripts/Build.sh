2e4e766934b718b6ac817727f45725a5dddee0d3
KieranPritchard <KieranPritchard06@gmail.com>
2024-11-02 12:13:37 +0000

    Added g++ to compile to the two variables

--- Devlopment Scripts/Build.sh
+++ Devlopment Scripts/Build.sh
@@ -4,3 +4,9 @@ filename = anyname
 executable_name = anyname.exe
 
 # G++ command compiles the above varibles to and executable file
+
+echo "$filename is now compiling." 
+
+g++ $filename -o $executable_name
+
+echo "Compiling of $filename now completed."
