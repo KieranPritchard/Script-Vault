df38a2c4eb1b487bc8725cd306d70d10348ef0b4
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-12-01 21:18:55 +0000

    Added main varibles to script.

--- File-Management-Scripts/Downloaded Files Organiser/Downloaded_Files_Organiser.py
+++ File-Management-Scripts/Downloaded Files Organiser/Downloaded_Files_Organiser.py
@@ -1,2 +1,6 @@
 import os
 import shutil
+
+# Main varibles to make the script work
+downloads_folder_path = "/Users/kieranpritchard/Downloads"
+sorted_files_location = "/Users/kieranpritchard/Documents/Sorted Downloads"
