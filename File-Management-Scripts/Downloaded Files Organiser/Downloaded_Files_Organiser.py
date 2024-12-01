cb0b9506313dca2973264653bd20733d26b4efcd
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-12-01 21:46:57 +0000

    Added comments and checks for images.

--- File-Management-Scripts/Downloaded Files Organiser/Downloaded_Files_Organiser.py
+++ File-Management-Scripts/Downloaded Files Organiser/Downloaded_Files_Organiser.py
@@ -7,6 +7,13 @@ sorted_files_location = "/Users/kieranpritchard/Documents/Sorted Downloads"
 
 # Loops through files to their file path
 for file in os.listdir(downloads_folder_path):
+    # Splits the text into a tuple and then gets the file extension.
     file_path = os.path.splitext(file)[1]
 
-    
+    # Checks for images in the downloads folder
+    if file_path == "jpg" or file_path == "png":
+        if os.path.exists("/Users/kieranpritchard/Documents/Sorted Downloads/Images"):
+            shutil.move(file,"/Users/kieranpritchard/Documents/Sorted Downloads/Images")
+        else:
+            os.makedirs("/Users/kieranpritchard/Documents/Sorted Downloads/Images")
+            shutil.move(file, "/Users/kieranpritchard/Documents/Sorted Downloads/Images")
