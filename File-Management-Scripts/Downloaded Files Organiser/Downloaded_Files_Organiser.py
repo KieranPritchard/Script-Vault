e9d691f1b76bc23db9ecac80f5d7fd54a4db8f2d
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-12-01 21:23:31 +0000

    Added main for loop to access the files in the downloads folder.

--- File-Management-Scripts/Downloaded Files Organiser/Downloaded_Files_Organiser.py
+++ File-Management-Scripts/Downloaded Files Organiser/Downloaded_Files_Organiser.py
@@ -4,3 +4,9 @@ import shutil
 # Main varibles to make the script work
 downloads_folder_path = "/Users/kieranpritchard/Downloads"
 sorted_files_location = "/Users/kieranpritchard/Documents/Sorted Downloads"
+
+# Loops through files to their file path
+for file in os.listdir(downloads_folder_path):
+    file_path = os.path.splitext(file)[1]
+
+    
