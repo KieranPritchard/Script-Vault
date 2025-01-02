f3944c44821f63ed158b6c03c96af4ea62b11002
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-01-02 21:58:51 +0000

    Added fix to build.sh script.

--- Devlopment-Scripts/Build-Script/Build.sh
+++ Devlopment-Scripts/Build-Script/Build.sh
@@ -11,7 +11,7 @@ filename=$1
 executable_name="${filename%.*}.exe"
 
 # Checks for if the file exists
-if [ ! -f "$filename"]; then
+if [ ! -f "$filename" ]; then
   echo "Error encountered: '$filename' does not exist."
   exit 1
 fi
