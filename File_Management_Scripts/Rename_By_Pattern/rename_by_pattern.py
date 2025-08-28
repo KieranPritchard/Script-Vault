b450d50352d9b035cf24c823c2ab56d64f5abdac
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-28 15:32:53 +0100

    laid out functions and imported modules

---
+++ File_Management_Scripts/Rename_By_Pattern/rename_by_pattern.py
@@ -0,0 +1,14 @@
+import os
+import rich
+
+def rename_by_file_type():
+    return
+
+def rename_by_directory_name():
+    return
+
+def main():
+    return 
+
+if __name__ == "__name__":
+    main()
