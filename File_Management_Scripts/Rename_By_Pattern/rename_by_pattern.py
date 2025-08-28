187f5fe1690998f17e22c498c4b613d3f1fe0d0f
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-28 15:50:23 +0100

    added rename by pattern

--- File_Management_Scripts/Rename_By_Pattern/rename_by_pattern.py
+++ File_Management_Scripts/Rename_By_Pattern/rename_by_pattern.py
@@ -1,5 +1,5 @@
 import os
-import rich
+from rich import print
 
 def rename_by_file_type():
     return
@@ -8,7 +8,26 @@ def rename_by_directory_name():
     return
 
 def main():
-    return 
+    print("=" * 60)
+    print("[bold]Rename by Pattern[/bold]")
+    while(True):
+        print("=" * 60)
+        print("[bold]Please select your option:[/bold]")
+        print("(1) Rename by file type.")
+        print("(2) Rename by directory name.")
+        print("(3) Quit.")
+        print("=" * 60)
+        user_option = int(input("Input your choice:"))
+        print("=" * 60)
 
-if __name__ == "__name__":
+        if user_option == 1:
+            rename_by_file_type()
+        elif user_option == 2:
+            rename_by_directory_name()
+        elif user_option == 3:
+            break
+        else:
+            print("Invaild Option")
+
+if __name__ == "__main__":
     main()
