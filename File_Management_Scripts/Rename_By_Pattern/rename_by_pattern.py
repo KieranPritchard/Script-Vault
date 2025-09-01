f385b375862c846c9adef517041c57f124a95dca
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-09-01 23:41:55 +0100

    fixed the function

--- File_Management_Scripts/Rename_By_Pattern/rename_by_pattern.py
+++ File_Management_Scripts/Rename_By_Pattern/rename_by_pattern.py
@@ -15,53 +15,36 @@ def rename_by_file_type():
 
     for file in track(os.listdir(folder), description="Renaming photos..."):
         file_path = os.path.join(folder, file)
-        if file_path[1] == "jpg" or file_path[1] == "jpeg" or file_path[1] == "png" or file_path[1] == "gif":
-            path_array = file.split("/")
-            path_array[len(path_array) -1] = f"photo_{photo_iterator}"
-            new_path = ""
-            for x in path_array:
-                new_path += f"/{x}"
-            os.rename(file, new_path)
+        ext = file_path.split(".")[-1].lower()
+        if ext == "jpg" or ext == "jpeg" or ext == "png" or ext == "gif":
+            new_file_name = f"photo_{photo_iterator}.{ext}"
+            new_file_path = os.path.join(folder, new_file_name)
+            os.rename(file_path, new_file_path)
             photo_iterator += 1
-        elif file_path[1] == "pdf" or file_path[1] == "docx" or file_path[1] == "doc" or file_path[1] == "txt" or file_path[1] == "odt" or file_path[1] == "rtf":
-            path_array = file.split("/")
-            path_array[len(path_array) -1] = f"photo_{photo_iterator}"
-            new_path = ""
-            for x in path_array:
-                new_path += f"/{x}"
-            os.rename(file, new_path)
+        elif ext == "pdf" or ext == "docx" or ext == "doc" or ext == "txt" or ext == "odt" or ext == "rtf":
+            new_file_name = f"document_{document_iterator}.{ext}"
+            new_file_path = os.path.join(folder, new_file_name)
+            os.rename(file_path, new_file_path)
             document_iterator += 1
-        elif file_path[1] == "xls" or file_path[1] == "xlsx" or file_path[1] == "csv" or file_path[1] == "ods":
-            path_array = file.split("/")
-            path_array[len(path_array) -1] = f"photo_{photo_iterator}"
-            new_path = ""
-            for x in path_array:
-                new_path += f"/{x}"
-            os.rename(file, new_path)
+        elif ext == "xls" or ext == "xlsx" or ext == "csv" or ext == "ods":
+            new_file_name = f"spreadsheet_{spreadsheet_iterator}.{ext}"
+            new_file_path = os.path.join(folder, new_file_name)
+            os.rename(file_path, new_file_path)
             spreadsheet_iterator += 1
-        elif file_path[1] == "ppt" or file_path[1] == "pptx" or file_path[1] == "odp":
-            path_array = file.split("/")
-            path_array[len(path_array) -1] = f"photo_{photo_iterator}"
-            new_path = ""
-            for x in path_array:
-                new_path += f"/{x}"
-            os.rename(file, new_path)
+        elif ext == "ppt" or ext == "pptx" or ext == "odp":
+            new_file_name = f"presentaition_{presentaition_iterator}.{ext}"
+            new_file_path = os.path.join(folder, new_file_name)
+            os.rename(file_path, new_file_path)
             presentaition_iterator += 1
-        elif file_path[1] == "mp3" or file_path[1] == "wav" or file_path[1] == "aac" or file_path[1] == "flac":
-            path_array = file.split("/")
-            path_array[len(path_array) -1] = f"photo_{photo_iterator}"
-            new_path = ""
-            for x in path_array:
-                new_path += f"/{x}"
-            os.rename(file, new_path)
+        elif ext == "mp3" or ext == "wav" or ext == "aac" or ext == "flac":
+            new_file_name = f"audio_{audio_iterator}.{ext}"
+            new_file_path = os.path.join(folder, new_file_name)
+            os.rename(file_path, new_file_path)
             audio_iterator += 1
-        elif file_path[1] == "mp4" or file_path[1] == "avi" or file_path[1] == "mov" or file_path[1] == "mkv" or file_path[1] == "wmv":
-            path_array = file.split("/")
-            path_array[len(path_array) -1] = f"photo_{photo_iterator}"
-            new_path = ""
-            for x in path_array:
-                new_path += f"/{x}"
-            os.rename(file, new_path)
+        elif ext == "mp4" or ext == "avi" or ext == "mov" or ext == "mkv" or ext == "wmv":
+            new_file_name = f"video_{video_iterator}.{ext}"
+            new_file_path = os.path.join(folder, new_file_name)
+            os.rename(file_path, new_file_path)
             video_iterator += 1
 
 def rename_by_directory_name():
