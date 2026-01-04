93a47dc38bde8b9da43074ed4bc245b1dedaa758
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-04 20:50:58 +0000

    fixed error

--- Reconnaissance_Scripts/website_metadata_scraper/website_metadata_scaper.py
+++ Reconnaissance_Scripts/website_metadata_scraper/website_metadata_scaper.py
@@ -7,9 +7,9 @@ import builtwith
 # Function to get random user agent from folder
 def get_random_agent():
     # Opens the file
-    with open("Script-Vault/Reconnaissance_Scripts/website_metadata_scraper/website_metadata_scaper.py", "r") as f:
+    with open("Script-Vault/Resources/user_agent_strings.txt", "r") as f:
         # Extracts the user agents as a list
-        user_agents = f.readlines()
+        user_agents = [ua.strip() for ua in f if ua.strip()]
 
     # Selects a random index
     user_agent = random.choice(user_agents)
