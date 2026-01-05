63eec9982187730b648b1148f21ab172dad54fa3
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-05 20:45:58 +0000

    added analyse robots function

--- Reconnaissance_Scripts/robots_sitemap_analyser/robots_sitemap_analyser.py
+++ Reconnaissance_Scripts/robots_sitemap_analyser/robots_sitemap_analyser.py
@@ -1,4 +1,6 @@
 import random
+from urllib.parse import urljoin
+import requests
 
 # Function to get random user agent from folder
 def get_random_agent():
@@ -12,3 +14,45 @@ def get_random_agent():
 
     # Returns the headers
     return {"User-Agent": user_agent}
+
+def analyse_robots(domain):
+    # Lists to store disallowed and sitemaps
+    disallowed = []
+    sitemaps = []
+    
+    # Gets random user agent for header
+    header = get_random_agent()
+
+    # Creates robots url
+    robots_url = urljoin(domain, "/robots.txt")
+
+    # Gets response from the request to the robots
+    reponse = requests.get(robots_url, headers=header, timeout=10)
+
+    # Gets the page content
+    contents = reponse.text
+
+    # Checks for if there is no contents in response
+    if not contents:
+        return disallowed, sitemap
+
+    # Loops over each line in contents after being split
+    for line in contents.splitlines():
+        # Strips the white space off of lines
+        line = line.strip()
+        
+        # turns the line into lowercase and checks if it is disallowed
+        if line.lower().startswith("disallow:"):
+            # Splites the line in halve one at the : and strips any remaining white space
+            path = line.split(":", 1)[1].strip()
+            # Adds disallowed to the path
+            disallowed.append(path)
+        # turns the line into lowercase and checks if it is a sitemap
+        elif line.lower().startswith("sitemap:"):
+            # Splites the line in halve one at the : and strips any remaining white space
+            sitemap = line.split(":", 1)[1].strip()
+            # Adds disallowed to the path
+            sitemaps.append(sitemap)
+    
+    # Returns the sitemap
+    return disallowed, sitemaps
