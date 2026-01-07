8bc7fc3bb9ddb833e87c3c28af4e8631af1454e1
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-07 22:05:28 +0000

    updated paths

--- Reconnaissance_Scripts/robots_sitemap_analyser/robots_sitemap_analyser.py
+++ Reconnaissance_Scripts/robots_sitemap_analyser/robots_sitemap_analyser.py
@@ -7,9 +7,9 @@ import time
 def load_wordlists():
     # Dictionary to store wordlist names and their file paths
     wordlist_paths = {
-        "config": "/usr/share/seclists/Discovery/Web-Content/Files/config-files.txt",
-        "backup": "/usr/share/seclists/Discovery/Web-Content/Files/backup-files.txt",
-        "git": "/usr/share/seclists/Discovery/Web-Content/Files/git-config.txt",
+        "config": "/usr/share/seclists/Discovery/Web-Content/web-all-content-types.txt",
+        "backup": "/usr/share/seclists/Discovery/Web-Content/Common-DB-Backups.txt",
+        "git": "/usr/share/seclists/Discovery/Web-Content/git-config.txt",
         "admin": "/usr/share/seclists/Discovery/Web-Content/CMS/admin-panels.txt",
         "api": "/usr/share/seclists/Discovery/Web-Content/api/api-endpoints.txt",
         "dev": "/usr/share/seclists/Discovery/Web-Content/Development/dev.txt",
