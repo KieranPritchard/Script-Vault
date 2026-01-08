6f1a7f139fa825bcc7f7475c43c95add14657c93
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-08 19:37:24 +0000

    created send arp function

--- Networking_Scripts/arp_network_discovery/arp_network_discovery.py
+++ Networking_Scripts/arp_network_discovery/arp_network_discovery.py
@@ -1,1 +1,15 @@
 from scapy import *
+
+# Function to send arp request to the network
+def send_arp(network):
+    # Creates an arp request packet
+    arp = ARP(pdst=network)
+    # Creates an Ethernet broadcast packet
+    ether = Ether(dst='ff:ff:ff:ff:ff:ff')
+    # Combines the two packets together
+    packet = ether / arp
+
+    # Send the packet and receive responses
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    return result
