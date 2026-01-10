e181015a9444f71af02b5a3daa8a8b16367132de
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-10 22:07:28 +0000

    added arguement checks

--- Networking_Scripts/banner_detection/banner_detection.sh
+++ Networking_Scripts/banner_detection/banner_detection.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+# checks for if there is a required arguement
+if [ -z "$1" ]; then
+    # Outputs usage guide and example of use
+    echo "Usage $0 <Target>"
+    echo "Example: $0 192.168.1.0"
+    # Exits the script
+    exit 1
+fi
+
+# Checks if nmap is installed
+if ! command -v nmap &> /dev/null; then
+    # Ouputs nmap isnt installed
+    echo "[-] nmap is not installed"
+    # Prompts the user to install nmap
+    echo "[!] Please install nmap"
+    # Exits the script
+    exit 1
+fi
