eec99d7e4ef66b2c47fa1f4beb37712848ac6f17
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-11 17:32:38 +0000

    added checks

--- Networking_Scripts/full_nmap_scan/full_nmap_scan.sh
+++ Networking_Scripts/full_nmap_scan/full_nmap_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check for root
+# Check for root privailages
 if [[ $EUID -ne 0 ]]; then
     echo "[!] This script must be run as root (OS detection requires it)."
     exit 1
@@ -23,4 +23,58 @@ if [ -z "$1" ]; then
     echo "Example: $0 192.168.1.0"
     # Exits the script
     exit 1
+fi
+
+# Stores variables
+TARGET="$1"
+MODE="$2"
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+OUTDIR="nmap_reports/quiet_vuln_${TARGET}_${DATE}"
+
+if [ "$MODE" = "save" ]; then
+    # Creates directiory to store nmap reports
+    mkdir -p "$OUTDIR"
+
+    # Ouputs the scan has started and is being saved
+    echo "[+] Starting Nmap scan against: $TARGET"
+    echo "[+] Reports will be saved to: $OUTDIR"
+    echo
+
+    # Run Nmap scan
+    nmap \
+        -p- \
+        -sS \
+        -sV \
+        -O \
+        -A \
+        --script=default,safe \
+        -T2 \
+        -oA "$OUTDIR/full_scan" \
+        "$TARGET"
+
+    echo
+    # Outputs successfull scan and the output files
+    echo "[✓] Scan completed successfully."
+    echo "[✓] Output files:"
+    echo "    - Normal:    $OUTDIR/full_scan.nmap"
+    echo "    - Grepable:  $OUTDIR/full_scan.gnmap"
+    echo "    - XML:       $OUTDIR/full_scan.xml"
+else
+    # Outputs the scan has started
+    echo "[+] Starting Nmap scan against: $TARGET"
+
+    # Gets the  results from the nmap scan
+    results=$(nmap -p- -sS -sV -O -A --script=default,safe -T2 "$TARGET" 2>/dev/null \
+    | grep "Nmap scan report for" \
+    | awk '{print $NF}')
+
+    # Checks if there isnt any results
+    if [ -z "$results" ]; then
+        # Outputs no live hosts found
+        echo "[-] No banners found."
+    else
+        # Outputs results
+        echo "[+] Banners found"
+        echo "$results"
+    fi
 fi
