3adf462b6c2f10e00219aec0a9bd727c1cd895c3
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-11 18:07:46 +0000

    created load targets function

--- Networking_Scripts/nmap_scan_target_file/nmap_scan_target_file.py
+++ Networking_Scripts/nmap_scan_target_file/nmap_scan_target_file.py
@@ -1,1 +1,15 @@
 import nmap
+
+# Function to open target file and load the targets
+def load_targets(file):
+    # Stores the targets in a list
+    targets = []
+    
+    # Opens the text file
+    with open(file, "r") as f:
+        # Loops over the file
+        for target in f:
+            # Adds the targets 
+            targets.append(target)
+
+    return targets
