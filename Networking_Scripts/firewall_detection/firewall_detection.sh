3c69b9e97d36444765eac9d5faad4bb718cc7d00
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-01-16 09:42:07 +0000

    added checks for nmap and root privalges

--- Networking_Scripts/firewall_detection/firewall_detection.sh
+++ Networking_Scripts/firewall_detection/firewall_detection.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+# Checks if nmap is installed
+if ! command -v nmap &> /dev/null; then
+    # Ouputs nmap isnt installed
+    echo "[-] nmap is not installed"
+    # Prompts the user to install nmap
+    echo "[!] Please install nmap"
+    # Exits the script
+    exit 1
+fi
+
+# Check for root privailages
+if [[ $EUID -ne 0 ]]; then
+    echo "[!] This script must be run as root (SYN scan requires it)."
+    exit 1
+fi
