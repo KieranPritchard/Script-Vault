2ed0f9f35eb1395e05e480e7b68a92c81244e3d5
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-02-07 17:23:54 +0000

    added random user agent function

--- Vulnerabilty_Detection/SQL_Detection/sql_injection_detection.py
+++ Vulnerabilty_Detection/SQL_Detection/sql_injection_detection.py
@@ -1,5 +1,19 @@
 import requests
 import time
+import random
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 import xml.etree.ElementTree as ET
+
+# Function to get random user agent from folder
+def get_random_agent():
+    # Opens the file
+    with open("../../Resources/user_agent_strings.txt", "r") as f:
+        # Extracts the user agents as a list
+        user_agents = [user_agent.strip() for user_agent in f if user_agent.strip()]
+
+    # Selects a random index
+    user_agent = random.choice(user_agents)
+
+    # Returns the headers
+    return {"User-Agent": user_agent}
